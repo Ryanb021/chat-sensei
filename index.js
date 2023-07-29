279a8af366eff95bc4f587902fd0652520a7bae5
Ryan Bagan <darcknight1980@gmail.com>
2023-07-29 05:41:02 -0700

    test working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express');
+const cors = require('cors');
 
 const configuration = new Configuration({
   organization: "org-2tjpSXMjpHzwOmuknaORiEZM",
@@ -7,22 +8,27 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-const port = 4000
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
+// const port = 4000;
 
 app.post('/', async (req, res) => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
-  });
+  const { message } = req?.body;
+  console.log(message);
+  // const response = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   prompt: "Say this is a test",
+  //   max_tokens: 7,
+  //   temperature: 0,
+  // });
 
-  console.log(response.data.choices[0].text)
+  // console.log(response.data.choices[0].text)
   res.json({
-    data: response.data
+    // data: response.data
+    data: message,
   })
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(4000, () => {
+  console.log('Server is running on port 4000')
 });
